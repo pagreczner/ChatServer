7c989f8f134d84730d4723c04561aa9092536f7e
Pete Greczner <pete@castleridgemedia.com>
2013-06-09 23:07:12 -0400

    some wholesale changes to the server

--- server/chat_server.js
+++ server/chat_server.js
@@ -1,8 +1,12 @@
 console.log('Starting Chat Server');
 
 var io = require('socket.io').listen(8080);
-var chatImageHash = {}
+var chatImageHash = {};
 var usersInRoom = {};
+var polls = [];
+var currentPoll = null;
+var voteLimiter = {};
+
 io.sockets.on('connection', function (socket) {
   
   
@@ -13,6 +17,7 @@ io.sockets.on('connection', function (socket) {
 		socket.emit('update_images', chatImageHash);
 		socket.emit('refresh_users', usersInRoom);
 		socket.set('uid',data['uid']);
+		socket.emit('new_poll', currentPoll);
 	});
   
   
@@ -21,10 +26,43 @@ io.sockets.on('connection', function (socket) {
   });
   
   
+  socket.on('create_poll', function(data){
+  	var password = data['password'];
+  	if(password == 'poll587'){
+  		createPoll(data['question'], data['choices']);
+  	}
+  });
+  socket.on('vote_for', function(data){
+  	var voteLimitObj = voteLimiter[data['uid']];
+  	var ok = true;
+  	if(voteLimitObj == null){
+  		voteLimiter[data['uid']] = new Date();
+  	} else {
+  		var diff = (new Date()).getTime() - voteLimitObj.getTime();
+  		if(diff < 30000){
+  			ok = false;
+  		} else {
+  			voteLimiter[data['uid']] = new Date();
+  		}
+  	}
+  	if(!ok)return;
+  	var i = 0;
+  	for(i = 0; i < currentPoll['choices'].length; i ++){
+  		if(currentPoll['choices'][i]['id'] == data['choice']){
+  			if(currentPoll['choices'][i]['votes'] == null){
+  				currentPoll['choices'][i]['votes'] = 0;	
+  			}
+  			currentPoll['choices'][i]['votes'] += 1;
+  		}
+  	}
+  	io.sockets.emit('update_poll', currentPoll['choices']);
+  });
+  
   socket.on('disconnect', function(){
   	socket.get('uid',function(err, uid){
   		delete chatImageHash[uid];
   		delete usersInRoom[uid];
+  		delete voteLimiter[uid];
   		socket.broadcast.emit('update_users', {uid: uid, images: chatImageHash});
   	});
   	
@@ -32,3 +70,16 @@ io.sockets.on('connection', function (socket) {
   
   
 });
+
+
+function createPoll(question, choices){
+	var pollObject = {
+		question: question,
+		alive: true,
+		choices: choices
+	};
+	
+	polls.push(pollObject);
+	currentPoll = pollObject;
+	io.sockets.emit('new_poll', pollObject);
+}
